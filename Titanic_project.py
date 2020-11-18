9ce4dd182d0a12879c2999793d6716cf7e7655d8
khatti53 <68162676+khatti53@users.noreply.github.com>
2020-11-18 15:41:00 +0530

    Delete Titanic_project.py
    delete titanic file

--- Titanic_project.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
-titanic_train = pd.read_csv('train.csv')
-titanic_test = pd.read_csv('test.csv')
-
